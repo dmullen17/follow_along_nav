32735b6300585c61469e97fdcdc9b2135d2eafd2
dmullen17 <dmullen17@gmail.com>
2019-01-29 15:04:47 -0800

    finished project 1

--- app.js
+++ app.js
@@ -5,19 +5,91 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 /* Define Functions */
-function highlightLink() {
-    // console.log(this.offsetHeight, this.offsetWidth);
-    const linkCoords = this.getBoundingClientRect();
-    console.log(linkCoords);
+function highlightLink(link) {
+    const linkCoords = link.getBoundingClientRect();
     
-    highlight.style.width = `${linkCoords.width}px`;
-    highlight.style.height = `${linkCoords.height}px`;
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        left: linkCoords.left + window.scrollX,
+        top: linkCoords.top + window.scrollY
+    }
     
-    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+function highlightLinkCallback() {
+    highlightLink(this);
+}
+
+function highlightLinkPageload() {
+    const home = document.querySelector('a[name="Home"]');
+    highlightLink(home)
 }
 
 /* Listen for a hover event on each link */
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink));
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLinkCallback));
+window.addEventListener('load', highlightLinkPageload);
+
+/* Define callback function using a closure */
+/*function callback(a) {
+  return function() {
+    console.log(a);
+  }
+}
+let x = 'hello world';
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', callback(x)));*/
+
+
+
+
+/* Get argument inputs to work.  This works!! */
+/*    function example(input) {
+    /*const selectedLink = input ? input : this;
+    console.log(selectedLink);*/
+    /*const link = this; 
+    console.log(link);
+   if (input !== null) {
+        const link = this;
+    } else {
+        const link = input;
+    }
+    const linkCoords = input.getBoundingClientRect();
+    
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        left: linkCoords.left + window.scrollX,
+        top: linkCoords.top + window.scrollY
+    }
+    
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+ Listen for a hover event on each link 
+const home = document.querySelector('a[name="Home"]');
+example(home);*/
+
+/*window.addEventListener('load', function() {
+    const home = document.querySelector('a[name="Home"]');
+    const linkCoords = home.getBoundingClientRect();
+    // console.log(linkCoords);
+    
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        left: linkCoords.left + window.scrollX,
+        top: linkCoords.top + window.scrollY
+    }
+    
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+});*/
 
 
 /* Troubleshooting */
