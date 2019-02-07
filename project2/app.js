9ea231bb6be0c6c0bbe97376331c2a1976269f88
dmullen17 <dmullen17@gmail.com>
2019-02-07 14:23:32 -0800

    finished project 2

--- project2/app.js
+++ project2/app.js
@@ -1,18 +1,40 @@
 /* Select DOM elements and define variables */
 const links = document.querySelectorAll('a');
 const highlighter = document.querySelector('.hl');
+const sectionHome = document.querySelector('section.home');
+// Unfortunately we need to hard code these
+const backgrounds = ['images/beyza-nur-kocaosmanoglu-1290279-unsplash.jpg',
+                    'images/clement-h-544786-unsplash.jpg',
+                    'images/dlanor-s-703975-unsplash.jpg',
+                    'images/med-badr-chemmaoui-630239-unsplash.jpg',
+                    'images/neringa-sidlauskaite-1247184-unsplash.jpg',
+                    'images/pankaj-patel-516695-unsplash.jpg',
+                    'images/roberto-nickson-1299738-unsplash.jpg'];
 
 /* Define functions */
 function highlight() {
     const coords = this.getBoundingClientRect();
     console.log(coords);
-    highlighter.style.top = `${coords.top}px`;
+    highlighter.style.top = `${coords.top + window.scrollY}px`;
     highlighter.style.width = `${coords.width}px`;
     highlighter.style.height = `${coords.height}px`;
-    highlighter.style.left = `${coords.x}px`;
+    highlighter.style.left = `${coords.x + window.scrollX}px`;
+}
+
+function setRandomBackground() {
+    const n = backgrounds.length;
+    const index = Math.floor((Math.random() * n));
+    //console.log(index);
+    sectionHome.style.background = `url(${backgrounds[index]})`;
+    sectionHome.style.backgroundPosition = 'center';
+    sectionHome.style.backgroundSize = 'cover';
 
 }
+setRandomBackground();
 
 
 /* Set up event listeners */
-links.forEach(link => link.addEventListener('mouseover', highlight));
+links.forEach(link => link.addEventListener('mouseenter', highlight));
+/*links.forEach(link => link.addEventListener('click', function() {
+    console.log('hi');
+}));*/
